ab755c5b3fbce5f1c4469ed7e7fa37408d1f71b3
oriaror <farraxovfarraxov@gmail.com>
2024-02-14 19:27:15 +0400

    mm2

--- index.js
+++ index.js
@@ -1,213 +1,162 @@
-"use strict"
+"use strict";
 
-const url = "https://api.opendota.com/api/heroes"
-let filter = []
-const backdrop = document.getElementById('backdrop')
+const url = "https://api.opendota.com/api/heroes";
+let filter = [];
+const backdrop = document.getElementById("backdrop");
 function generateHero(name, atribute) {
-  const heroes = document.querySelector('.heroes')
-  const heroDiv = document.createElement('div')
-  const img = document.createElement('img')
-  const heroP = document.createElement('p')
+  const heroes = document.querySelector(".heroes");
+  const heroDiv = document.createElement("div");
+  const img = document.createElement("img");
+  const heroP = document.createElement("p");
   heroDiv.onclick = (event) => {
-    backdropStyle()
-    generateModal(event)
-  }
-  img.setAttribute('src', getHeroImgPath(name))
-  img.classList.add('img')
-  heroDiv.appendChild(img)
-  heroDiv.appendChild(heroP)
-  heroes.appendChild(heroDiv)
-  heroP.innerHTML = name
-  heroDiv.classList.add('hero_container')
-  heroDiv.classList.add(atribute)
-  heroDiv.classList.add('box')
-  heroP.classList.add('hero_name')
+    backdropStyle();
+    generateModal(event);
+  };
+  img.setAttribute("src", getHeroImgPath(name));
+  img.classList.add("img");
+  heroDiv.appendChild(img);
+  heroDiv.appendChild(heroP);
+  heroes.appendChild(heroDiv);
+  heroP.innerHTML = name;
+  heroDiv.classList.add("hero_container");
+  heroDiv.classList.add(atribute);
+  heroDiv.classList.add("box");
+  heroP.classList.add("hero_name");
 }
 
 function backdropStyle() {
-  document.body.style.overflow = 'hidden'
-  backdrop.style.display = 'block'
+  document.body.style.overflow = "hidden";
+  backdrop.style.display = "block";
 }
 
 function generateModal(event) {
-  const modalName = document.querySelector('.modal_name')
-  modalName.innerHTML = event.currentTarget.children[1].innerText
+  const modalName = document.querySelector(".modal_name");
+  modalName.innerHTML = event.currentTarget.children[1].innerText;
 }
 
-
 function getHeroImgPath(name) {
-  return 'img/' + name.toLowerCase().replace(/[^a-z]/ig, '') + '.png'
+  return "img/" + name.toLowerCase().replace(/[^a-z]/gi, "") + ".png";
 }
 
 let request = async function (url) {
-  const response = await fetch(url).then(response => {
-    return response.json()
-  }).then(data => {
-    data.forEach(hero => {
-      generateHero(hero.localized_name, hero.primary_attr)
+  const response = await fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((hero) => {
+        generateHero(hero.localized_name, hero.primary_attr);
+      });
+    })
+    .then(() => {
+      filter = document.querySelectorAll(".box");
     });
-  }).then(() => {
-    filter = document.querySelectorAll('.box')
-  })
-  return response
-}
-request(url)
+  return response;
+};
+request(url);
 
 function changeFilterValue(event) {
-  let filterClass = event.currentTarget.dataset.f
-  filter.forEach(elem => {
-    elem.classList.remove('hide')
-    if (!elem.classList.contains(filterClass) && filterClass !== 'uni') {
-      elem.classList.add('hide')
+  let filterClass = event.currentTarget.dataset.f;
+  filter.forEach((elem) => {
+    elem.classList.remove("hide");
+    if (!elem.classList.contains(filterClass) && filterClass !== "uni") {
+      elem.classList.add("hide");
     }
-  })
+  });
 }
 
 function closeModal() {
-  backdrop.style.display = 'none'
-  document.body.style.overflow = 'auto'
-  const slots = document.getElementsByClassName('column')
-  Array.from(slots).forEach(element => {
-    element.innerHTML = null
-  })
-  const buildDesc = document.querySelector('.build_name')
-  buildDesc.innerHTML = null
+  backdrop.style.display = "none";
+  document.body.style.overflow = "auto";
+  const slots = document.getElementsByClassName("column");
+  Array.from(slots).forEach((element) => {
+    element.innerHTML = null;
+  });
+  const buildDesc = document.querySelector(".build_name");
+  buildDesc.innerHTML = null;
 }
 
 function fillSlots() {
-  const buildDesc = document.querySelector('.build_name')
-  buildDesc.innerHTML = null
-  buildDesc.innerHTML += ' через' + buildName()
+  const buildDesc = document.querySelector(".build_name");
+  buildDesc.innerHTML = null;
+  buildDesc.innerHTML += " через" + buildName();
   getImgId(61).forEach((el, index) => {
-    document.getElementById(`item${index + 1}`).innerHTML = el
-  })
+    document.getElementById(`item${index + 1}`).innerHTML = el;
+  });
 }
 
 function randomNumber(max) {
-  let set = new Set()
+  let set = new Set();
   for (let i = 0; i < max; i++) {
-    set.add(Math.floor(Math.random() * max))
+    set.add(Math.floor(Math.random() * max));
   }
-  return Array.from(set).slice(0, 6)
+  return Array.from(set).slice(0, 6);
 }
 
 function getImgId(max) {
-  let arr = randomNumber(max)
-  let srcArr = []
+  let arr = randomNumber(max);
+  let srcArr = [];
   for (let i = 0; i < arr.length; i++) {
-    const id = `<img src = 'artifacts/${arr[i]}.webp'>`
-    srcArr.push(id)
+    const id = `<img src = 'artifacts/${arr[i]}.webp'>`;
+    srcArr.push(id);
   }
-  return srcArr
+  return srcArr;
 }
 
 function buildName() {
-  const arr1 = [' адский', ' ржавый', ' черный', ' секретный', ' анальный', ' потный', ' лютый', ' угарный', ' фашистский', ' бесконечный', ' быстрый', ' тактический', ' пивной', ' ZXC', ' терпильный', ' хардкорный']
-  const arr2 = [' запой', ' мангус', ' юмор', ' айтем билд', ' позор', ' фид', ' снос трона', ' сон', ' стон', ' радик?', ' пулемет', ' алкоголь', ' трах', ' кач', ' скам', ' тролл фейс', ' рейдж', ' фарм', ' импакт', ' интеллект']
-  let str = []
+  const arr1 = [
+    " адский",
+    " ржавый",
+    " черный",
+    " секретный",
+    " потный",
+    " лютый",
+    " угарный",
+    " бесконечный",
+    " быстрый",
+    " тактический",
+    " пивной",
+    " ZXC",
+    " терпильный",
+    " хардкорный",
+  ];
+  const arr2 = [
+    " запой",
+    " мангус",
+    " юмор",
+    " айтем билд",
+    " позор",
+    " фид",
+    " снос трона",
+    " сон",
+    " стон",
+    " радик?",
+    " пулемет",
+    " алкоголь",
+    " кач",
+    " скам",
+    " тролл фейс",
+    " рейдж",
+    " фарм",
+    " импакт",
+    " интеллект",
+  ];
+  let str = [];
 
   for (let i = 0; i < arr1.length; i++) {
-    let first = Math.floor(Math.random() * arr1.length)
-    str.push(arr1[first])
-    break
+    let first = Math.floor(Math.random() * arr1.length);
+    str.push(arr1[first]);
+    break;
   }
 
   for (let i = 0; i < arr2.length; i++) {
-    let second = Math.floor(Math.random() * arr1.length)
-    str.push(arr2[second])
-    break
+    let second = Math.floor(Math.random() * arr1.length);
+    str.push(arr2[second]);
+    break;
   }
 
-  return str.join('')
+  return str.join("");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ///////first try//////
 
@@ -226,7 +175,6 @@ function buildName() {
 
 // request()
 
-
 //////second try///////
 
 // const request = fetch(url);
@@ -235,7 +183,6 @@ function buildName() {
 //     generateHero(item.localized_name, item.primary_attr)
 //   });
 // })
-
 
 //////third try//////
 
